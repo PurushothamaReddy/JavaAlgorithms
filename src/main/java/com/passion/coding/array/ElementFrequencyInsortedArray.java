36a612f6ab39a584235e25e8088b23bfafda80d6
Purushtham <purush.scjp@gmail.com>
2022-09-03 12:35:59 +0530

    commit check

--- src/main/java/com/passion/coding/array/ElementFrequencyInsortedArray.java
+++ src/main/java/com/passion/coding/array/ElementFrequencyInsortedArray.java
@@ -5,7 +5,7 @@ public class ElementFrequencyInsortedArray {
         int[] arr = {1,2, 2, 2, 3, 3, 4, 5, 5, 6, 6};
         //approach 2 use hashmap
         int prev=arr[0],curr=arr[0];
-        int freqcount=0;
+        int freqcount = 0;
         for (int i=0;i<arr.length;i++){
             curr=arr[i];
             if(prev != curr){
