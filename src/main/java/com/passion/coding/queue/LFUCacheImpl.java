d21438329300ea5afc009f24b79a29445920ddf5
purushotham <purush.scjp@gmail.com>
2023-12-06 08:05:29 +0800

    LRu cache

---
+++ src/main/java/com/passion/coding/queue/LFUCacheImpl.java
@@ -0,0 +1,49 @@
+package com.passion.coding.queue;
+
+import java.util.HashMap;
+import java.util.Map;
+//https://algodaily.com/lessons/implement-an-lfu-cache/java
+public class LFUCacheImpl {
+
+    private Map<Integer, Integer> valueMap = new HashMap<>();
+    private Map<Integer, Integer> countMap = new HashMap<>();
+    private int capacity;
+
+    public LFUCacheImpl(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public Integer get(int key) {
+
+        if (!valueMap.containsKey(key)) {
+            return -1;
+        }
+        countMap.put(key, countMap.get(key) + 1);
+        return valueMap.get(key);
+    }
+
+    public void put(Integer key, Integer value) {
+        if (valueMap.size() == capacity) {
+
+            int min = Integer.MAX_VALUE;
+            int lowestFreqKey =0;
+            for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
+                if (entry.getValue() < min) {
+                    min = entry.getValue();
+                    lowestFreqKey = entry.getKey();
+                }
+            }
+
+            valueMap.remove(lowestFreqKey);
+            countMap.remove(lowestFreqKey);
+        }
+
+        if (valueMap.containsKey(key)) {
+            countMap.put(key,countMap.get(key)+1);
+        } else {
+            countMap.put(key,1);
+        }
+        valueMap.put(key, value);
+    }
+
+}
